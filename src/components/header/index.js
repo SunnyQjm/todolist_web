b634429c2367dcbc994787b6726f028a61fffa3f
SunnyQjm <qjm253@gmail.com>
2018-09-12 13:16:17 +0800

    修改header，加入登陆和注册Modal

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
-
-import Menu from 'antd/lib/menu';
-import Icon from 'antd/lib/icon';
-import Dropdown from 'antd/lib/dropdown';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import {
-    Link
-} from 'react-router-dom'
 import LocalRouter from '../../LocalRouter'
+import {
+    Modal,
+    Button,
+    Form,
+    Icon,
+    Input,
+    Checkbox
+} from 'antd';
 import {
     BaseColor
 } from '../base/base-component';
@@ -42,16 +43,70 @@ const WebTitle = styled.div`
     font-weight: bold;
     text-align: center;
     padding: 0;
+    flex-grow: 0;
 `;
+
+const FlexDiv = styled.div`
+    flex-grow: 1;
+`;
+
+const FormItem = Form.Item;
 
 class Nav extends React.Component {
 
-    constructor(props, context) {
-        super(props, context);
+    static MODE = {
+        LOGIN: 'login',
+        REGISTER: 'register',
+    };
+
+    state = {
+        modalVisible: false,
+    };
+
+    constructor(props) {
+        super(props);
+        this.showRegister = this.showRegister.bind(this);
+        this.showLoginModal = this.showLoginModal.bind(this);
+        this.hideModal = this.hideModal.bind(this);
     }
 
+    showLoginModal() {
+        this.setState({
+            modalVisible: true,
+            mode: Nav.MODE.LOGIN
+        })
+    }
+
+    showRegister() {
+        this.setState({
+            modalVisible: true,
+            mode: Nav.MODE.REGISTER
+        })
+    }
+
+    hideModal() {
+        this.setState({
+            modalVisible: false
+        })
+    }
+
+    /**
+     * 处理表单提交
+     * @param e
+     */
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
+
     render() {
-        const {isMobile, selectedKey} = this.props;
+        const {isMobile} = this.props;
+        const {mode} = this.state;
+        const {getFieldDecorator} = this.props.form;
         let searchComponent = <SearchComponent placeholder={'搜索资源'} onSearch={(value) => {
             this.props.history.push(`/search/${value}`)
         }}/>;
@@ -63,19 +118,84 @@ class Nav extends React.Component {
                         height: '30px',
                     }}/>
                 </Logo>
-                <WebTitle>Todo List</WebTitle>
-                {
-                    isMobile ?
-                        searchComponent
-                        :
-                        ''
-                }
                 {
                     isMobile ?
                         ''
                         :
-                        searchComponent
+                        <WebTitle>Todo List</WebTitle>
                 }
+                <FlexDiv/>
+
+                {searchComponent}
+
+                <a type="primary" onClick={this.showLoginModal}>登陆</a>
+                <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+                <a type="primary" onClick={this.showRegister}>注册</a>
+
+                <Modal
+                    title={mode === Nav.MODE.LOGIN ? "登陆" : "注册"}
+                    centered
+                    visible={this.state.modalVisible}
+                    footer={null}
+                    onCancel={this.hideModal}
+                >
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <FormItem>
+                            {getFieldDecorator('username', {
+                                rules: [{required: true, message: '请输入用户名！'}],
+                            })(
+                                <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                       placeholder="用户名"/>
+                            )}
+                        </FormItem>
+                        <FormItem>
+                            {getFieldDecorator('password', {
+                                rules: [{required: true, message: '请输入密码！'}],
+                            })(
+                                <Input prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>} type="password"
+                                       placeholder="密码"/>
+                            )}
+                        </FormItem>
+                        {
+                            mode === Nav.MODE.REGISTER ?
+                                <FormItem>
+                                    {getFieldDecorator('passwordAgain', {
+                                        rules: [{required: true, message: '请再次请输入密码！'}],
+                                    })(
+                                        <Input prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                               type="password"
+                                               placeholder="再次输入密码"/>
+                                    )}
+                                </FormItem>
+                                :
+                                ""
+                        }
+                        <FormItem>
+                            {
+                                mode === Nav.MODE.LOGIN ?
+                                    getFieldDecorator('remember', {
+                                        valuePropName: 'checked',
+                                        initialValue: true,
+                                    })(
+                                        <Checkbox>记住密码</Checkbox>
+                                    )
+                                    :
+                                    ""
+                            }
+                            <Button type="primary" htmlType="submit" className="login-form-button" style={{
+                                width: '100%'
+                            }}>
+                                {mode === Nav.MODE.LOGIN ? "登陆" : "注册"}
+                            </Button>
+                            {
+                                mode === Nav.MODE.LOGIN ?
+                                    <span>Or <a onClick={this.showRegister}>注册</a></span>
+                                    :
+                                    <span>Or <a onClick={this.showLoginModal}>登陆</a></span>
+                            }
+                        </FormItem>
+                    </Form>
+                </Modal>
             </Header>
         );
     }
@@ -84,15 +204,13 @@ class Nav extends React.Component {
 Nav.propTypes = {
     isMobile: PropTypes.bool,
     defaultSelectedKey: PropTypes.string,
-    onSelect: PropTypes.func,
 };
 
 Nav.defaultProps = {
     isMobile: false,
     defaultSelectedKey: LocalRouter.HOME,
-    onSelect: (item, key, selectedKeys) => {
-    }
 };
 
+const WrappedNav = Form.create()(Nav);
 
-export default Nav;
+export default WrappedNav;
